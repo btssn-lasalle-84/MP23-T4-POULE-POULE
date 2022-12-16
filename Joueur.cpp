d86eb65d03f6827844b4fc8e1eeb9ffe7b39c8aa
guillaumetflorent <florent.guillaumet.pro@gmail.com>
2022-12-16 11:14:06 +0100

    MP23T4-3 Implémente la Joueur

---
+++ Joueur.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <string>
+#include "Joueur.h"
+
+using namespace std;
+
+Joueur::Joueur() : nomJoueur(" ")
+{
+
+}
+
+Joueur::Joueur(string nomJoueur) : nomJoueur(nomJoueur)
+{
+
+}
---
+++ Joueur.h
@@ -0,0 +1,18 @@
+#ifndef JOUEUR_H
+#define JOUEUR_H
+
+#include <iostream>
+#include <string>
+
+class Joueur
+{
+    private:
+        std::string nomJoueur;
+
+    public:
+        Joueur();
+        Joueur(std::string nomJoueur);
+        std::string getNomJoueur() const;
+};
+
+#endif // JOUEUR_H
