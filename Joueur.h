bede335cbbb42b4104e8d06dbe04116a6ef86aff
guillaumetflorent <florent.guillaumet.pro@gmail.com>
2022-12-17 15:21:05 +0100

    MP23T4-3 Ajoute l'association avec la classe MaitrePoulePoule ainsi que les accesseurs get/set

--- Joueur.h
+++ Joueur.h
@@ -4,15 +4,19 @@
 #include <iostream>
 #include <string>
 
+class MaitrePoulePoule;
 class Joueur
 {
     private:
+        MaitrePoulePoule* leMaitrePoulePoule;
         std::string nomJoueur;
 
     public:
         Joueur();
         Joueur(std::string nomJoueur);
         std::string getNomJoueur() const;
+        MaitrePoulePoule* getMaitrePoulePoule();
+        void setMaitrePoulePoule(MaitrePoulePoule* maitrePoulePoule);
 };
 
 #endif // JOUEUR_H
