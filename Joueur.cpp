9dd41bd23f248553f2aeb5c7b5df16be01064aa2
guillaumetflorent <florent.guillaumet.pro@gmail.com>
2023-01-04 16:06:17 +0100

    MP23T4-3 clang format

--- Joueur.cpp
+++ Joueur.cpp
@@ -5,10 +5,13 @@ using namespace std;
 
 Joueur::Joueur() : nomJoueur(" ")
 {
-
 }
 
 Joueur::Joueur(string nomJoueur) : nomJoueur(nomJoueur)
 {
+}
 
+std::string Joueur::getNomJoueur() const
+{
+    return nomJoueur;
 }
--- Joueur.h
+++ Joueur.h
@@ -4,13 +4,13 @@
 #include <string>
 class Joueur
 {
-    private:
-        std::string nomJoueur;
+  private:
+    std::string nomJoueur;
 
-    public:
-        Joueur();
-        Joueur(std::string nomJoueur);
-        std::string getNomJoueur() const;
+  public:
+    Joueur();
+    Joueur(std::string nomJoueur);
+    std::string getNomJoueur() const;
 };
 
 #endif // JOUEUR_H
