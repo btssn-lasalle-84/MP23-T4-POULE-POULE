83f829ad0e88512a1a227d2daf7a832498f79ff8
bcochet <batcochet2@gmail.com>
2023-01-04 14:14:33 +0100

    MP23T4-4 squelette de la classe Carte et definition du constructeur par defaut, d initialisation et du destructeur

--- Carte.cpp
+++ Carte.cpp
@@ -1,5 +1,4 @@
 #include "Carte.h"
-#include <iostream>
 #include <string>
 
 Carte::Carte() : valeurCarte(0)
@@ -7,7 +6,7 @@ Carte::Carte() : valeurCarte(0)
 }
 
 Carte::Carte(std::string symbole, unsigned int valeurCarte) :
-                            symbole(symbole), valeurCarte(valeurCarte)
+    symbole(symbole), valeurCarte(valeurCarte)
 {
 }
 
--- Carte.h
+++ Carte.h
@@ -1,23 +1,23 @@
 #ifndef CARTE_H
 #define CARTE_H
 
-#include <iostream>
 #include <string>
 
-#define NB_CARTES 36
-class MaitrePoulePoule;
+#define NB_CARTES_OEUF   15
+#define NB_CARTES_POULE  10
+#define NB_CARTES_RENARD 10
+#define NB_CARTES_COQ    1
 
 class Carte
 {
-    private:
-        MaitrePoulePoule *maPoule;
-        std::string symbole;
-        unsigned int valeurCarte;
+  private:
+    std::string  symbole;
+    unsigned int valeurCarte;
 
-    public:
-        Carte();
-        Carte(std::string symbole, unsigned int valeurCarte);
-        ~Carte();
+  public:
+    Carte();
+    Carte(std::string symbole, unsigned int valeurCarte);
+    ~Carte();
 };
 
 #endif
