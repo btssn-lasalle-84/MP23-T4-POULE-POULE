62df3086df691dd5563012aae003cb8adaeb3c27
bcochet <batcochet2@gmail.com>
2023-01-05 14:37:37 +0100

    MP23T4-5 Definie et declare les constructeurs de classe

---
+++ MaitrePoulePoule.h
@@ -0,0 +1,16 @@
+#ifndef MAITREPOULEPOULE_H
+#define MAITREPOULEPOULE_H
+
+#include <string>
+
+class MaitrePoulePoule
+{
+  private:
+    unsigned int NbPointJoueur;
+
+  public:
+    MaitrePoulePoule();
+    MaitrePoulePoule(unsigned int NbPointJoueur);
+};
+
+#endif
